57e9384a41ce7189f5b995d5aae93b85ebaab998
Sebastian Burgstaller <sebastian.burgstaller@gmail.com>
2017-09-15 21:40:59 +0900

    imports and index.html adapted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+import {
+    Router,
+    Routes
+} from '@angular/router';
+import {SearchComponent} from "./search/search.component";
+import {PaperComponent} from "./paper/paper.component";
+import {RouteDef} from "./menubar-item/menubar-item.component";
+
+let routeDef: RouteDef[] = [
+    {label: 'Paper Search', name: 'Root', path: '', component: SearchComponent },
+    {label: 'Paper Overview', name: 'paper_data', path: 'paper_data/:qid', component: PaperComponent },
+];
+
+export const routes: Routes = [
+    { path: '', component: SearchComponent, pathMatch: 'full' },
+    { path: 'paper_data/:qid', component: PaperComponent, pathMatch: 'full' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    routeDef: RouteDef[];
+
+    constructor(private router: Router) {
+        this.routeDef = routeDef;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+    RouterModule,
+    Routes
+} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent, routes,} from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import {APP_BASE_HREF, HashLocationStrategy, LocationStrategy} from "@angular/common";
+import {SearchComponent} from "./search/search.component";
+import {MenubarItemComponent} from "./menubar-item/menubar-item.component";
+import { PaperComponent } from './paper/paper.component';
+import { DataTableComponent } from './data-table/data-table.component';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import {SearchBoxComponent, SearchResultService, WDQService} from './search/search-box/search-box.component';
+import { SearchResultComponent } from './search/search-result/search-result.component';
+import { SearchResultTableComponent } from './search/search-result-table/search-result-table.component';
+import { RefTimelineComponent } from './paper/ref-timeline/ref-timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+      AppComponent,
+      MenuBarComponent,
+      SearchComponent,
+      MenubarItemComponent,
+      PaperComponent,
+      // DataTableComponent,
+      SearchBoxComponent,
+      SearchResultComponent,
+      SearchResultTableComponent,
+      RefTimelineComponent,
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      RouterModule.forRoot(routes),
+      Ng2SmartTableModule,
   ],
-  providers: [],
+  providers: [
+      { provide: APP_BASE_HREF,    useValue: '/' },
+      { provide: LocationStrategy, useClass: HashLocationStrategy },
+      { provide: SearchResultService, useClass: SearchResultService },
+      { provide: WDQService, useClass: WDQService },
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

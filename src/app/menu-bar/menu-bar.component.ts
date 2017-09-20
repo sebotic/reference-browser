504fb09dcc39d9d041e259dc9ae77081b0dbc0f7
Sebastian Burgstaller <sebastian.burgstaller@gmail.com>
2017-09-20 10:24:57 -0700

    added code to replace css semantic-ui menu bar with real menu bar

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -15,3 +15,35 @@ export class MenuBarComponent implements OnInit {
     }
 
 }
+
+// import { Component, OnInit, Input } from '@angular/core';
+// import {RouteDef} from "../menubar-item/menubar-item.component";
+//
+// import {
+//     Router,
+//     ActivatedRoute
+// } from '@angular/router';
+// import { Location } from '@angular/common';
+//
+// @Component({
+//     selector: 'app-menu-bar',
+//     templateUrl: './menu-bar.component.html',
+//     styleUrls: ['./menu-bar.component.css']
+// })
+// export class MenuBarComponent implements OnInit {
+//     @Input('items') items: RouteDef[];
+//     @Input('item') item: RouteDef;
+//
+//     constructor(private router: Router,
+//                 private route: ActivatedRoute,
+//                 private location: Location) {
+//     }
+//
+//     isActive(): boolean {
+//         return `/${this.item.path}` === this.location.path();
+//     }
+//
+//     ngOnInit() {
+//     }
+//
+// }
